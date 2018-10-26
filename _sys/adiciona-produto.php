150507b78103e6121fcba9c98dfce1d3755e91a0
b4_erickson <ericksonppu@gmail.com>
2018-10-26 18:19:24 -0300

    inserindo produto

--- _sys/adiciona-produto.php
+++ _sys/adiciona-produto.php
@@ -4,12 +4,12 @@ $nome_produto=$_POST['nome_produto'];
 $preco_produto=$_POST['preco_produto'];
 $quantidade_produto=$_POST['quantidade_produto'];
 
-//var_dump($quantidade_produto,$nome_produto,$preco_produto);
+//var_dump($nome_produto,$preco_produto,$quantidade_produto);
 if (insereProduto($conexao,$nome_produto,$preco_produto,$quantidade_produto)){
     header("Location:../public/lista_produto.php?cadastro=1");
     die();
 }else{
-  header("Location:../public/form_produto.php");
+  //header("Location:../public/form_produto.php");
 ?>
     <p class="alert-danger" >
 <?php
--- _sys/consulta_produto.php
+++ _sys/consulta_produto.php
@@ -2,8 +2,8 @@
 include "../conexao/conecta.php";
 function insereProduto($conexao,$nome_produto,$preco_produto,$quantidade_produto){
     $query = "insert into Produtos(Nome_Produto, Preco_Produto, Quantidade_Produto) values
- ({$nome_produto},{$preco_produto},{$quantidade_produto})";
+ ('{$nome_produto}',{$preco_produto},{$quantidade_produto})";
 
-    //var_dump($query);
+   // var_dump($query);
     return mysqli_query($conexao,$query);
 }
